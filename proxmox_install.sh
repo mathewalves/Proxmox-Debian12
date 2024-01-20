3417d34f7128004e09fbaa03824e2c539e885f78
Mateus Alves <135336248+mathewalves@users.noreply.github.com>
2024-01-20 10:39:31 -0300

    first_commit
    teste

--- proxmox_install.sh
+++ proxmox_install.sh
@@ -6,7 +6,8 @@ if [ "$(whoami)" != "root" ]; then
 fi
 
 echo "Bem-vindo ao script de instalação do Proxmox no Debian 12 Bookworm"
-echo "Este script também instalará alguns pacotes adicionais, como 'nala', 'net-tools', 'nmap' e 'neofetch'."
+echo "Este script também perguntará se você deseja instalar alguns pacotes adicionais, 
+como 'nala', 'neofetch' e pacotes de ferramentas de rede: 'net-tools' e 'nmap'."
 
 read -p "Deseja iniciar a instalação? (Digite 'sim' para continuar): " resposta
 
@@ -18,19 +19,67 @@ fi
 echo "Iniciando a instalação..."
 
 # Instalando pacotes
-echo "Instalando 'nala'..."
-apt install -y nala
+# nala
+read -p "Deseja instalar o 'nala'? (Opcional) (Digite 'sim' para instalar): " resposta_nala
 
-echo "Instalando 'neofetch'..."
-nala install -y neofetch
+if [ "$resposta_nala" == "sim" ]; then
+    echo "Iniciando a instalação do 'nala'..."
+    apt install -y nala
+    echo "'nala' instalado com sucesso."
+else
+    echo "Continuando a instalação sem 'nala'..."
+fi
 
-echo "Instalando 'net-tools'..."
-nala install -y net-tools
+read -p "Deseja instalar o 'neofetch'? (Opcional) (Digite 'sim' para instalar): " resposta_neofetch
 
-echo "Instalando 'nmap'..."
-nala install -y nmap
+if [ "$resposta_neofetch" == "sim" ]; then
+    if [ "$resposta_nala" == "sim" ]; then
+        echo "Iniciando a instalação do 'neofetch' com o nala..."
+        nala install -y neofetch
+        echo "'neofetch' instalado com sucesso."
+    else
+        echo "Iniciando a instalação do 'neofetch' com o apt..."
+        apt install -y neofetch
+        echo "'neofetch' instalado com sucesso."
+    fi
+else
+    echo "Continuando a instalação sem o 'neofetch'..."
+fi
+
+# ferramentas de rede
+read -p "Deseja instalar as ferramentas de rede 'net-tools' e 'nmap'? (Opcional) (Digite 'sim' para instalar): " resposta_net
+if [ "$resposta_net" == "sim" ]; then
+    if [ "$resposta_nala" == "sim" ]; then
+        echo "Iniciando a instalação do 'net-tools' & 'nmap' com o nala..."
+        nala install -y nmap && nala install -y net-tools
+        echo "Pacotes instalados com sucesso."
+    else
+        echo "Iniciando a instalação do 'net-tools' & 'nmap' com o apt..."
+        apt install -y nmap && apt install -y net-tools
+        echo "Pacotes instalados com sucesso."
+    fi
+else
+    echo "Continuando a instalação sem as ferramentas de rede..."
+fi
 
 # Comandos de instalação do Proxmox
+echo iniciando instalação do Proxmox
+
+# Solicitar ao usuário o nome do host e o endereço IP
+read -p "Digite o nome do host: " hostname
+read -p "Digite o endereço IP: " ip_address
+
+# Verificar se o arquivo /etc/hosts já contém uma entrada para o nome do host
+if grep -q "$hostname" /etc/hosts; then
+    echo "O nome do host '$hostname' já está presente no arquivo /etc/hosts."
+    exit 1
+fi
+
+# Adicionar a nova entrada ao arquivo /etc/hosts
+echo "$ip_address    $hostname" | sudo tee -a /etc/hosts > /dev/null
+
+echo "Entrada adicionada com sucesso ao arquivo /etc/hosts:"
+cat /etc/hosts | grep "$hostname"
 
 # Substitua este comentário pelos comandos reais de instalação do Proxmox e outros pacotes
 
