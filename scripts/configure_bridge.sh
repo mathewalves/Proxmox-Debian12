01cf45cc68376bdae6ae79ad65b69174b6ead9e6
Mateus Alves <135336248+mathewalves@users.noreply.github.com>
2024-01-23 16:35:08 -0300

    Update configure_bridge.sh

--- scripts/configure_bridge.sh
+++ scripts/configure_bridge.sh
@@ -6,6 +6,19 @@ if [ "$(whoami)" != "root" ]; then
     exit $?
 fi
 
+verificar_bridge()
+{
+    # Verificar se a bridge vmbr0 já existe
+                    if grep -q "iface vmbr0" /etc/network/interfaces; then
+                        echo "A bridge vmbr0 já existe. Reconfigurando..."
+                        
+                        # Remover a configuração existente
+                        sed -i '/iface vmbr0/,/^$/d' /etc/network/interfaces
+                    else
+                        echo "Criando a bridge vmbr0..."
+                    fi
+}
+
 bridge()
 {
     # Caminho para o arquivo de configuração
@@ -28,46 +41,71 @@ bridge()
     echo "Máscara de Sub-rede: $MASCARA_SUBREDE"
     echo "Gateway: $GATEWAY"
 
+    verificar_bridge
+
     # Utilizar as variáveis lidas do arquivo ou solicitar novas se estiverem em branco
-    echo -e "Deseja editar as informações da interface de rede para criação da bridge?"
-    echo -e "([S]im para editar [N]ão para continuar com a interface listada acima)"
-    read -p "Resposta: " editar
+    echo -e "Configurando as informações da interface de rede para criação da bridge..."
+    PS3="Selecione uma opção (Digite o número): "
+    options=("Configurar Manualmente" "Usar DHCP" "Sair")
 
-    if [[ "$editar" =~ ^[Ss](im)?$ ]]; then
-        read -p "Informe o nome da interface física (deixe em branco para manter $INTERFACE): " interface_fisica
-        read -p "Informe o endereço IP para a bridge (deixe em branco para manter $IP_ADDRESS): " endereco_ip
-        read -p "Informe a máscara de sub-rede para a bridge (deixe em branco para manter $MASCARA_SUBREDE): " mascara_subrede
-        read -p "Informe o gateway para a bridge (deixe em branco para manter $GATEWAY): " gateway
-    fi
+    select opt in "${options[@]}"; do
+        case $opt in
+            "Configurar Manualmente")
+                # Leitura manual das configurações
+                read -p "Informe o nome da interface física (deixe em branco para manter): " interface_fisica
+                read -p "Informe o endereço IP para a bridge (deixe em branco para manter): " endereco_ip
+                read -p "Informe a máscara de sub-rede para a bridge (deixe em branco para manter): " mascara_subrede
+                read -p "Informe o gateway para a bridge (deixe em branco para manter): " gateway
 
-    # Utilizar as variáveis lidas ou as novas informadas
-    INTERFACE=${interface_fisica:-$INTERFACE}
-    IP_ADDRESS=${endereco_ip:-$IP_ADDRESS}
-    MASCARA_SUBREDE=${mascara_subrede:-$MASCARA_SUBREDE}
-    GATEWAY=${gateway:-$GATEWAY}
+                    # Utilizar as variáveis lidas ou as novas informadas
+                    INTERFACE=${interface_fisica:-$INTERFACE}
+                    IP_ADDRESS=${endereco_ip:-$IP_ADDRESS}
+                    MASCARA_SUBREDE=${mascara_subrede:-$MASCARA_SUBREDE}
+                    GATEWAY=${gateway:-$GATEWAY}
 
-
-# Verificar se a bridge vmbr0 já existe
-if grep -q "iface vmbr0" /etc/network/interfaces; then
-    echo "A bridge vmbr0 já existe. Reconfigurando..."
-    
-    # Remover a configuração existente
-    sed -i '/iface vmbr0/,/^$/d' /etc/network/interfaces
-else
-    echo "Criando a bridge vmbr0..."
-fi
 
 # Criar a bridge vmbr0 com as novas informações
 cat <<EOF >> /etc/network/interfaces
 auto vmbr0
 iface vmbr0 inet static
     address $IP_ADDRESS
-    netmask $MASCARA_SUBREDE
+#    netmask $MASCARA_SUBREDE
     gateway $GATEWAY
     bridge_ports $INTERFACE
     bridge_stp off
     bridge_fd 0
 EOF
+
+                break 2
+                ;;
+            "Usar DHCP")
+                # Configuração para DHCP
+                # Criar a bridge vmbr0 com as novas informações
+cat <<EOF >> /etc/network/interfaces
+auto vmbr0
+iface vmbr0 inet dhcp
+    bridge_ports $INTERFACE
+    bridge_stp off
+    bridge_fd 0
+EOF
+                    
+                break 2
+                ;;
+            "Sair")
+                echo "Saindo..."
+                exit 0
+                ;;
+            *) echo "Opção inválida";;
+        esac
+    done
+
+# Comentar as configurações da interface física no arquivo de configuração
+if [ -n "$interface_fisica" ]; then
+    sed -i "/iface $INTERFACE inet static/,/iface/ s/^/#/" /etc/network/interfaces
+    sed -i "/iface $INTERFACE inet dhcp/,/iface/ s/^/#/" /etc/network/interfaces
+fi
+
+
 
     # Reiniciar o serviço de rede para aplicar as alterações
     echo "Reiniciando o serviço de rede..."
